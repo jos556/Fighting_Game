04ba6edc98a33bae60a8c4db058423c20e513908
LAPTOP-9IRE96SJ\88690 <d25823095@gmail.com>
2024-11-09 01:35:01 +0800

    Initial commit: Project setup with React + Vite

---
+++ README.md
@@ -0,0 +1,13 @@
+# React 格鬥遊戲 (React Fighting Game)
+
+這是一個使用 React + Vite 開發的網頁格鬥遊戲。
+
+## 技術棧
+
+- React 18
+- Vite
+- TypeScript
+- CSS Modules
+- Zustand (狀態管理)
+
+## 專案結構 
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React Fighting Game</h1>
+      <div className="game-container">
+        {/* 遊戲組件將在這裡渲染 */}
+      </div>
+    </div>
+  )
+}
+
+export default App 
